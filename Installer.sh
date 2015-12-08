0ec180635344de4be71d11eac1d297c23237a345
Daniel Binggeli <db@xbe.ch>
2015-12-08 19:25:04 +0100

    added some comments

--- Installer.sh
+++ Installer.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # This script will help you install nvidia drivers and bumblebee
 
-echo "Type the nvidia driver Version you like to install. [ENTER] example: nvidia-355"
+read -p "Type the nvidia driver Version you like to install. [ENTER] example: nvidia-355" nvididriver
 
-read nvididriver
+if echo "$nvididriver" | grep -iq "nvidia-" ;then
 
-if ($nvididriver); then
+  echo "** Remove old nvidia drivers**"
   apt-get remove --purge nvidia*
-  sudo apt-get remove --purge bumblebee*
+  apt-get remove --purge bumblebee*
+
+  echo "** install basics**"
   apt-get --purge remove xserver-xorg-video-nouveau
-  apt-get install linux-source && sudo apt-get install linux-headers-$(uname -r)
+  apt-get install linux-source && apt-get install linux-headers-$(uname -r)
   
-  echo "Backup the blacklist.conf to blacklist.conf.bak"
+  echo "** Backup the blacklist.conf to blacklist.conf.bak **"
   cp /etc/modprobe.d/blacklist.conf /etc/modprobe.d/blacklist.conf.bak
-  echo "add the nvidia drivers to the blacklist"
+  echo "** add the nvidia drivers to the blacklist **"
   echo "# Necessary to install nvidia drivers" >> /etc/modprobe.d/blacklist.conf
   echo "blacklist nouveau" >> /etc/modprobe.d/blacklist.conf
   echo "blacklist lbm-nouveau" >> /etc/modprobe.d/blacklist.conf
@@ -27,15 +29,18 @@ if ($nvididriver); then
   echo "alias lbm-nouveau off" >> /etc/modprobe.d/blacklist.conf
   echo "options nouveau modeset=0" >> /etc/modprobe.d/blacklist.conf
   
+  echo "** Install the nvidia driver **"
   apt-get update && apt-get dist-upgrade -y
   apt-get install $nvididriver-updates nvidia-settings
-  
+
+  echo "** remove nvidia-prime **"
   apt-get remove --purge nvidia-prime
   
+  echo "** Install the bumblebee driver **"
   apt-get install bumblebee bumblebee-nvidia virtualgl virtualgl-libs virtualgl-libs-ia32:i386 virtualgl-libs:i386
-  sudo usermod -a -G bumblebee $USER
+  usermod -a -G bumblebee $USER
   
-  
+  echo "** configure the bumblebee driver **"
   cd /etc/bumblebee
   ls -Al
   
@@ -47,7 +52,7 @@ if ($nvididriver); then
   cd /tmp/
   git clone https://github.com/Bumblebee-Project/bumblebee-ui.git
   cd bumblebee-ui
-  sudo ./INSTALL
+  ./INSTALL
    
 else
   echo "not a driver"
--- README.md
+++ README.md
@@ -8,6 +8,6 @@ This script will install all the packages you need and configure the settings yo
 # Howto
 Download the installer.sh file.
 Execute with root priviledges and give the driver you wish as param.
-
+```bash
   sudo ./installer.sh
-
+```
